d093fbc222cfa5806f48c2d952098adb90d61778
bar-85 <bajwolskabarbara@gmail.com>
2022-12-08 21:22:19 +0100

    Add LogoNavbar.js and LogoNavbar.styles

---
+++ src/components/LogoNavbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Logo } from '../styles/components/LogoNavbar.styles'
+import { IoLogoFirebase } from "react-icons/io5";
+
+const LogoNavbar = () => {
+	return <Logo>React FIREBASE <IoLogoFirebase className='ioLogo'/> </Logo>
+ 
+}
+
+export default LogoNavbar
---
+++ src/styles/components/LogoNavbar.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Logo = styled.div`
+	font-size: 2rem;
+	font-weight: bold;
+	letter-spacing: 0.05rem;
+
+    .ioLogo {
+        color: orange;
+    }
+
+    @media screen and (max-width: 900px) {
+		font-size: 1.3rem;
+		
+	}
+`
