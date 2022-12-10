95181dfcac7b3c8ce51ac8606cd6e4cfc618c278
bar-85 <bajwolskabarbara@gmail.com>
2022-12-10 21:10:50 +0100

    Add styles to Auth.styles

--- src/styles/pages/Auth.styles.js
+++ src/styles/pages/Auth.styles.js
@@ -1,1 +1,67 @@
+import styled from 'styled-components'
 
+export const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 80vh;
+`
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	max-width: 500px;
+	width: 100%;
+`
+export const Box = styled.div`
+	width: 100%;
+	padding: 50px;
+	border: 1px solid black;
+	border-radius: 10px;
+	box-shadow: 5px 2px 15px black;
+
+	h2 {
+		text-transform: uppercase;
+	}
+
+	input {
+		width: 100%;
+		padding: 10px;
+		margin: 10px auto;
+		outline: none;
+		font-size: 16px;
+		border: 1px solid rgb(69, 0, 68);
+		border-radius: 4px;
+	}
+
+	div {
+		width: 100%;
+		border: 1px solid rgb(69, 0, 68);
+		padding: 10px;
+		text-align: center;
+		margin: 10px auto;
+		border-radius: 4px;
+		background-color: rgb(232, 226, 226);
+		cursor: pointer;
+	}
+
+	p {
+		cursor: pointer;
+		color: purple;
+		font-size: 16px;
+		margin: 25px auto;
+		font-weight: bold;
+	}
+
+	button {
+		width: 100%;
+		border: 1px solid rgb(69, 0, 68);
+		padding: 10px;
+		text-align: center;
+		margin: 10px auto;
+		border-radius: 4px;
+		background-color: rgb(69, 0, 68);
+		color: white;
+		cursor: pointer;
+		font-size: 16px;
+	}
+`
