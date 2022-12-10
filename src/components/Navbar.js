19a9636984dec3cbafbd4dbfd7bdcf8ab4ae358d
bar-85 <bajwolskabarbara@gmail.com>
2022-12-10 21:12:21 +0100

    Add Logout and react-toastify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,24 @@ import React, { useState } from 'react'
 import { Wrapper, Nav, Hamburger, Logout } from '../styles/components/Navbar.styles'
 import LogoNavbar from '../components/LogoNavbar'
 import { FaBars, FaTimes } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+import { auth } from '../firebase'
+import { signOut } from 'firebase/auth'
 
 const Navbar = () => {
 	const [click, setClick] = useState(false)
 	const handleClick = () => setClick(!click)
 
 	const closeMenu = () => setClick(false)
+
+	const logoutFunc = async () => {
+		toast.success('Następuje wylogowanie...')
+		await signOut(auth)
+		setTimeout(() => {
+			window.location = '/'
+		}, 5000)
+
+	}	
 
 	return (
 		<Wrapper>
@@ -16,9 +28,11 @@ const Navbar = () => {
 				{click ? <FaTimes size={32} style={{ color: 'white' }} /> : <FaBars size={32} style={{ color: 'white' }} />}
 			</Hamburger>
 			<Nav className={click ? 'header-menu active' : 'header-menu'}>
+				<div onClick={logoutFunc} >
 				<Logout className='navbar-right' onClick={closeMenu}>
 					Wyloguj
 				</Logout>
+				</div>
 			</Nav>
 		</Wrapper>
 	)
