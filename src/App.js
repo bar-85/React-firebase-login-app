ea5d5229db7b2f3fc8ef2b8ac6745284c32700db
bar-85 <bajwolskabarbara@gmail.com>
2022-12-10 21:14:31 +0100

    Add react-tastify and auth firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Auth from './pages/Auth';
-import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+import Navbar from './components/Navbar'
+import Auth from './pages/Auth'
+import Home from './pages/Home'
+import 'react-toastify/dist/ReactToastify.css'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-				<Navbar />
+	const [users, setUsers] = useState(null)
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+		  if (user) {
+			const uid = user.uid;
+			console.log(uid)
+			setUsers(user)
+			// ...
+		  } else {
+		  }
+		});
+	  }, [])
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				{users?.accessToken && <Navbar />}
 				<Routes>
 					<Route path='/' element={<Auth />} />
 					<Route path='dashboard' element={<Home />} />
 				</Routes>
 			</BrowserRouter>
-    </div>
-  );
+			<ToastContainer position='top-right' autoClose={5000} theme='light' />
+			<ToastContainer />
+		</div>
+	)
 }
 
-export default App;
+export default App
