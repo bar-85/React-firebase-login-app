4f950685031aef9329738d723bbf7f24a1b72616
bar-85 <bajwolskabarbara@gmail.com>
2022-12-10 21:22:18 +0100

    Add ScrollToTop

--- src/App.js
+++ src/App.js
@@ -8,29 +8,32 @@ import 'react-toastify/dist/ReactToastify.css'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
 import { useEffect, useState } from 'react'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 function App() {
 	const [users, setUsers] = useState(null)
 
 	useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-		  if (user) {
-			const uid = user.uid;
-			console.log(uid)
-			setUsers(user)
-			// ...
-		  } else {
-		  }
-		});
-	  }, [])
+		onAuthStateChanged(auth, user => {
+			if (user) {
+				const uid = user.uid
+				console.log(uid)
+				setUsers(user)
+				// ...
+			} else {
+			}
+		})
+	}, [])
 	return (
 		<div className='App'>
 			<BrowserRouter>
-				{users?.accessToken && <Navbar />}
-				<Routes>
-					<Route path='/' element={<Auth />} />
-					<Route path='dashboard' element={<Home />} />
-				</Routes>
+				<ScrollToTop>
+					{users?.accessToken && <Navbar />}
+					<Routes>
+						<Route path='/' element={<Auth />} />
+						<Route path='dashboard' element={<Home />} />
+					</Routes>
+				</ScrollToTop>
 			</BrowserRouter>
 			<ToastContainer position='top-right' autoClose={5000} theme='light' />
 			<ToastContainer />
