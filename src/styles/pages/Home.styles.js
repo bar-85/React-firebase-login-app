a11178cf7fc454f5b28f6c14c04898e1ffe2998b
bar-85 <bajwolskabarbara@gmail.com>
2022-12-11 21:37:27 +0100

    Add styles

---
+++ src/styles/pages/Home.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Welcome = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 30px;
+	height: 80vh;
+
+	span {
+		font-weight: bold;
+		margin-left: 10px;
+	}
+`
