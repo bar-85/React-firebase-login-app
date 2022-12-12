6ce983093082988c947e5952935752be8ff8b4f7
bar-85 <bajwolskabarbara@gmail.com>
2022-12-12 22:01:59 +0100

    Add style

--- src/styles/pages/Home.styles.js
+++ src/styles/pages/Home.styles.js
@@ -4,11 +4,26 @@ export const Welcome = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	font-size: 30px;
+	font-size: 1.5rem;
 	height: 80vh;
+	
+	div {
+		margin-bottom: 0.5rem;
+	}
 
 	span {
 		font-weight: bold;
-		margin-left: 10px;
+		margin: 1rem;
+		color: rgb(182, 52, 179);
 	}
+
+	@media screen and (max-width: 900px) {
+		font-size: 1rem;
+		
+	}
+`
+export const Wrapper = styled.div`
+	padding: 10px;
+	border-radius: 10px;
+	box-shadow: 0px 3px 5px rgb(69, 0, 68);
 `
